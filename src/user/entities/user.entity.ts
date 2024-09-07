514ac5abb5314daf5088b32dc49ea52968fd6160
diego97jaramillo <diego97jaramillo@gmail.com>
2024-09-06 21:11:36 -0500

    User entity created

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("users")
+export class User {
+    @PrimaryGeneratedColumn("uuid")
+    id: string
+
+    @Column("text", {})
+    username: string
+
+    @Column("text")
+    name: string
+
+    @Column("text")
+    lastname: string
+
+    @Column("text")
+    password: string
+}
