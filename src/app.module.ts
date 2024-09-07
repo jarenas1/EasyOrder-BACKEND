55480b965d51eaabf4c2f213502fca5f5d0b8d72
diego97jaramillo <diego97jaramillo@gmail.com>
2024-09-06 20:18:30 -0500

    configuraciones linkeadas con las variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configurations from './common/config/configurations';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({
+    load: [configurations],
+    isGlobal: true
+  }),
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => ({
+      type: 'postgres',
+      host: configService.get('database.host'),
+      port: +configService.get('database.port'),
+      username: configService.get('database.user'),
+      password: configService.get('database.password'),
+      database: configService.get('database.database'),
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+  })]
 })
 export class AppModule {}
---
+++ src/common/config/configurations.ts
@@ -0,0 +1,10 @@
+export default () => ({
+    port: parseInt(process.env.PORT, 10) || 3008,
+    database: {
+      host: process.env.POSTGRES_HOST,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      port: parseInt(process.env.POSTGRES_PORT, 10) || 5430
+    }
+  });
