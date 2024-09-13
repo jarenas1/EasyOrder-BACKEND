429464100fcc88f28a9a649f189afe59853d4e2a
CrisGos <cristianortega9509@gmail.com>
2024-09-13 17:49:42 -0500

    pre-guard

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,7 +7,7 @@ export class UserService {
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
-
+  
   findAll() {
     return `This action returns all user`;
   }
