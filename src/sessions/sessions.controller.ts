391addf4b3721495ba5670af53c9fed6ab0f2d1f
CrisGos <cristianortega9509@gmail.com>
2024-09-16 19:25:08 -0500

    creating tables and sessions Swagger

--- src/sessions/sessions.controller.ts
+++ src/sessions/sessions.controller.ts
@@ -7,16 +7,24 @@ import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guard/role.guard';
 import { RolesEnum } from 'src/common/enums';
 import { CreateSessionDto } from './dto/create-session.dto';
+import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
 
+@ApiTags('Sessions')
+@ApiBearerAuth() // Para JWT auth en Swagger
 @Controller('sessions')
 export class SessionsController {
   constructor(private readonly sessionsService: SessionsService) {}
 
+  @ApiOperation({ summary: 'Crear una nueva sesión' })
+  @ApiResponse({ status: 201, description: 'Sesión creada exitosamente.' })
+  @ApiResponse({ status: 400, description: 'Datos inválidos.' })
   @Post()
   create(@Body() session: CreateSessionDto) {
     return this.sessionsService.create(session);
   }
 
+  @ApiOperation({ summary: 'Obtener todas las sesiones' })
+  @ApiResponse({ status: 200, description: 'Lista de todas las sesiones.' })
   @RoleDecorator(RolesEnum.mesero)
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Get()
@@ -24,11 +32,17 @@ export class SessionsController {
     return this.sessionsService.findAll();
   }
 
+  @ApiOperation({ summary: 'Obtener sesiones no pagadas' })
+  @ApiResponse({ status: 200, description: 'Lista de sesiones no pagadas.' })
+  @ApiResponse({ status: 404, description: 'No se encontraron sesiones no pagadas.' })
   @Get('unpaid')
   findUnpaid() {
     return this.sessionsService.findUnpaid();
   }
 
+  @ApiOperation({ summary: 'Actualizar el estado de pago de una sesión' })
+  @ApiResponse({ status: 200, description: 'Estado de la sesión actualizado.' })
+  @ApiResponse({ status: 404, description: 'Sesión no encontrada.' })
   @Patch(':id/paid')
   async updatePaid(
     @Param('id') id: string,
