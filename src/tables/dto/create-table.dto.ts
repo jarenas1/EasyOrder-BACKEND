712517e628e42a196322f4f2e5a9ba2558afef3f
diego97jaramillo <diego97jaramillo@gmail.com>
2024-09-19 20:57:57 -0500

    le seteo el id del user al table

--- src/tables/dto/create-table.dto.ts
+++ src/tables/dto/create-table.dto.ts
@@ -1,5 +1,6 @@
 import { IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { User } from 'src/user/entities/user.entity';
 
 export class CreateTableDto {
   @ApiProperty({ description: 'El nombre de la mesa', example: 'Mesa 1' })
@@ -9,4 +10,7 @@ export class CreateTableDto {
   @ApiProperty({ description: 'El estado de la mesa', example: 'disponible' })
   @IsString()
   status: string;
+
+  @IsString()
+  user: User;
 }
