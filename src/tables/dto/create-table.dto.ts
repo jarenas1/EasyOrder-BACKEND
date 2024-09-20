246a4e8ddaab28751e59981f692097e32c41d96b
diego97jaramillo <diego97jaramillo@gmail.com>
2024-09-19 20:58:28 -0500

    le seteo el id del user al table

--- src/tables/dto/create-table.dto.ts
+++ src/tables/dto/create-table.dto.ts
@@ -1,4 +1,4 @@
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { User } from 'src/user/entities/user.entity';
 
